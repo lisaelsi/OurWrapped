947347ab06d60e1bf480f186ef7ccc42952672ff
lisaelsi <lisasamuelssons@gmail.com>
2022-11-25 14:23:53 +0100

    added refreshing of token

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import json
 import os
 import requests
 import base64
-from secrets import CREATE_PLAYLIST_TOKEN, SPOTIFY_USER_ID, GET_PLAYLIST_TOKEN, GET_TRACKS_TOKEN, CLIENT_SECRET, CLIENT_ID
+from secrets import SPOTIFY_USER_ID as user_id
+from refresh import Refresh
 
 # Create new playlist OurWrapped
     # TODO --> set playlist description as usernames of collaborators! 
@@ -19,28 +20,6 @@ from secrets import CREATE_PLAYLIST_TOKEN, SPOTIFY_USER_ID, GET_PLAYLIST_TOKEN, 
 
 playlist_name = "Nu är det höst"
 
-# ----- copied code ------- #
-# Step 1 - Authorization 
-url = "https://accounts.spotify.com/api/token"
-headers = {}
-data = {}
-
-# Encode as Base64
-message = f"{CLIENT_ID}:{CLIENT_SECRET}:"
-print(message)
-messageBytes = message.encode('ascii')
-base64Bytes = base64.b64encode(messageBytes)
-base64Message = base64Bytes.decode('ascii')
-
-headers['Authorization'] = f"Basic {base64Message}"
-data['grant_type'] = "client_credentials"
-
-r = requests.post(url, headers=headers, data=data)
-
-token = r.json()['access_token']
-
-
-# -------------------------------------------- # 
 
 def create_playlist():
     # Create playlist on spotify 
@@ -51,7 +30,7 @@ def create_playlist():
     })
 
     query = "https://api.spotify.com/v1/users/{}/playlists".format(
-        SPOTIFY_USER_ID)
+        user_id)
     response = requests.post(
         query,
         data=request_body,
@@ -68,7 +47,7 @@ def get_user_playlists():
     limit = 5
     offset = 10
     query = "https://api.spotify.com/v1/users/{}/playlists?limit={}&offset={}".format(
-        SPOTIFY_USER_ID,
+        user_id,
         limit, 
         offset)
     response = requests.get(
@@ -83,13 +62,13 @@ def get_user_playlists():
 
 def get_user_playlist_id(response, playlist_name):
     playlists = response.get('items')
-    
+
     for p in playlists:
         if p.get("name") == playlist_name:
             return(p.get('id'))
-    
+
     print('Playlist not found')
-            
+
         # ------  TODO - error handling ------
 
 def get_tracks_from_playlist(no_of_tracks, playlist_id):
@@ -114,7 +93,7 @@ def get_tracks_from_playlist(no_of_tracks, playlist_id):
 
 def get_track_ids(no_of_tracks, playlist_id):
     tracks_dict = get_tracks_from_playlist(no_of_tracks, playlist_id)
-    
+
     tracks = tracks_dict.get('items')
 
     track_ids = [item['track']['id'] for item in tracks]
@@ -128,14 +107,21 @@ def add_track_to_playlist(track_ids):
     # add track to ourwrapped 
     pass
 
-if __name__ == '__main__':
-    new_playlist_info = create_playlist()
-    print(new_playlist_info)
+def call_refresh():
+    refresh_caller = Refresh()
+    print('Token refreshed')
+    return(refresh_caller.refresh())
+ 
 
+if __name__ == '__main__':
+
+    token = call_refresh()
+
+    #new_playlist_info = create_playlist()
+    #print(new_playlist_info)
     no_of_tracks = 3
 
     #user_playlists = get_user_playlists()
     #playlist_id = get_user_playlist_id(user_playlists, playlist_name)
     #track_ids = get_track_ids(no_of_tracks, playlist_id)
-
     #get_playlists_id(new_playlist_info)
